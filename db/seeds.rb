56a94ac5eec53256fd0719c68f720134c52bb379
dom alexander <dom.alexan@gmail.com>
2023-01-03 21:31:45 -0700

    Adds more users to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 puts "🌱 Planting seeds..."
 
-u1 = User.create(first_name: "Leeroy", last_name: "Jenkins", birthday: 1989-06-24, clan: "World of Warcraft")
+u1 = User.create(first_name: "Leeroy", last_name: "Jenkins", birthday: 1969-07-08, clan: "World of Warcraft")
+u2 = User.create(first_name: "Dana", last_name: "White", birthday: 1989-06-24, clan: "Fallout")
+u3 = User.create(first_name: "Bilbo", last_name: "Baggins", birthday: 1800-04-25, clan: "LOTR")
+u4 = User.create(first_name: "Siouxsie", last_name: "Sioux", birthday: 1957-05-27, clan: "GOT")
 
 rec1 = Recipe.create(title: "", series: "", image_url: "", prep_time: "", ingredients: "", instructions: "")
 rec2 = Recipe.create(title: "", series: "", image_url: "", prep_time: "", ingredients: "", instructions: "")
